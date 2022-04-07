8782eba0f1ec8ed982f092b1c58fb5af241d8967
yusufdinc1812 <103222694+yusufdinc1812@users.noreply.github.com>
2022-04-08 02:11:00 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,6 +2,7 @@ public class TestClass {
     public static void main(String[] args) {
 
         System.out.println("Git den merhaba!");
+         System.out.println("GitHub dan merhaba!");
 
 
     }
